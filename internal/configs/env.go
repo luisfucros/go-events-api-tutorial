281c34920da4ff12cd133a9345abc111ff730628
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-10-21 23:42:20 +0000

    update file

--- internal/configs/env.go
+++ internal/configs/env.go
@@ -10,7 +10,7 @@ import (
 
 type Config struct {
 	PublicHost             string
-	Port                   string
+	Port                   int64
 	DBUser                 string
 	DBPassword             string
 	DBAddress              string
@@ -26,7 +26,7 @@ func initConfig() Config {
 
 	return Config{
 		PublicHost:             getEnv("PUBLIC_HOST", "http://localhost"),
-		Port:                   getEnv("PORT", "8080"),
+		Port:                   getEnvAsInt("PORT", 8080),
 		DBUser:                 getEnv("DB_USER", "root"),
 		DBPassword:             getEnv("DB_PASSWORD", "events_password"),
 		DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
