ba2e3509bf9ba7a5a0c8e87a7c4b6b7a458c1e78
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-11-04 22:45:43 +0000

    add user context

---
+++ cmd/api/context.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luisfucros/go-events-api-tutorial/internal/database"
+)
+
+func (app *application) getUserFromContext(c *gin.Context) *database.User {
+	contextUser, exist := c.Get("user")
+	if !exist {
+		return &database.User{}
+	}
+	user, ok := contextUser.(*database.User)
+	if !ok {
+		return &database.User{}
+	}
+
+	return user
+}
